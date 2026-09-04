abeb7487fa9cb1453c19d0cf9dd3637aeeaf1bac
Vansh <vanshchoudhary2744@gmail.com>
2026-09-04 12:37:38 +0530

    manipulating DOM using useRef

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import Counting from "./components/Counting";
 function App() {
   const [count, setCount] = useState(0);
   const value = useRef(0);
+  const color = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    value.current += 1;
+    value.current = value.current + 1;
   });
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -15,9 +16,18 @@ function App() {
         <p>{count}</p>
         <button onClick={() => setCount((prev) => prev - 1)}>-</button>
       </div>
-      <div>
+      <div ref={color}>
         <h1>{value.current}</h1>
       </div>
+      <button
+        onClick={() => {
+          if (color.current) {
+            color.current.style.backgroundColor = "cyan";
+          }
+        }}
+      >
+        Change Color
+      </button>
     </div>
   );
 }
